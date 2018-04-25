7ef63f1e40944c85a9cf8bf780a42143713e9911
Alex Hirsch <w4rh4wk@bluephoenix.at>
2018-04-25 09:20:05 +0200

    Add defaults.cmake

---
+++ cmake/defaults.cmake
@@ -0,0 +1,12 @@
+if(NOT CMAKE_BUILD_TYPE)
+	set(CMAKE_BUILD_TYPE "Release" CACHE STRING "CMake Build Type" FORCE)
+endif()
+
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+set(_cxx_compiler_ids GNU Clang AppleClang)
+if(CMAKE_CXX_COMPILER_ID IN_LIST _cxx_compiler_ids)
+	set(CMAKE_CXX_FLAGS "-Wall -Wextra ${CMAKE_CXX_FLAGS}")
+endif()
