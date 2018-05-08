8e6df27acaf2ded23ad219d7bf5dd90c9c8c070d
Alex Hirsch <w4rh4wk@bluephoenix.at>
2018-05-08 14:42:09 +0200

    Ignore unused parameter warnings

--- cmake/defaults.cmake
+++ cmake/defaults.cmake
@@ -8,5 +8,5 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 
 set(_cxx_compiler_ids GNU Clang AppleClang)
 if(CMAKE_CXX_COMPILER_ID IN_LIST _cxx_compiler_ids)
-	set(CMAKE_CXX_FLAGS "-Wall -Wextra ${CMAKE_CXX_FLAGS}")
+	set(CMAKE_CXX_FLAGS "-Wall -Wextra -Wno-unused-parameter ${CMAKE_CXX_FLAGS}")
 endif()
